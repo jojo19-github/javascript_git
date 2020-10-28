192b824caa01145be0283663cb5f59dca4638bd7
Josiane Mendy <mendyjosiane19@gmail.com>
2020-10-28 16:22:46 +0000

    javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var fruits = [];
+fruits[0]= "Banane";
+fruits[1]= "Orange";
+fruits[2]= "Mangue";
+fruits[3]= "Pomme";
+fruits[4]= "Poire";
+
+
+var info ={};
+info.Name='Banane'
+info.color='Jaune'
+info.texture='Solide'
+info.taste='Sucré'
+
+var info ={};
+info.Name='Orange';
+info.color='Orange';
+info.texture='Jus';
+info.taste='Sucré,acide';
